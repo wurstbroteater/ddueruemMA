c1600b469c7b023dea9d2f14e1cab9b85ac61938
Eric <elangenbucher@deloitte.com>
2022-09-11 11:47:55 +0200

    WIP Clustering

--- svo/pre_cl_family.py
+++ svo/pre_cl_family.py
@@ -62,71 +62,41 @@ def run(fm, seed=None, **kwargs):
     features_with_cluster = []
     for feature in features:
         feature = feature.copy()
-        feature.update({'cluster': {'features': set(), 'relations': list()}})
+        # {'clusters': [{'features': set(), 'relations': list()}, ...]
+        feature.update({'clusters': list()})
         features_with_cluster.append(feature)
 
-    print('first', features_with_cluster[0])
+    # print('first', features_with_cluster[0])
+    # f1 = [x for x in features if x['name'] == 'Friendly'][0]
+    # f2 = [x for x in features if x['name'] == 'Warrior'][0]
+    # t_lca = find_lca(f1, f2, root, features_with_cluster)
+    # print("t_lca", t_lca)
+    # print("roots", list(map(lambda x: x['name'], roots(f1, f1, t_lca, features_with_cluster))))
+    # print("roots", roots(f1, f1, t_lca, features_with_cluster))
+
     # print('first in feature', features[0])
     for clause in ctc_clauses:
         pairs = get_distinct_pairs(clause)
         for f1, f2 in pairs:
             pair = (f1, f2)
-            ancestor = find_lca(f1, f2, root, features)
-            print('pair', (f1['name'], f2['name']))
-            print('lca for', (f1['name'], f2['name']), 'is', ancestor['name'])
-            feature_with_cluster = [f for f in features_with_cluster if
-                                    f['name'] == ancestor['name'] and len(set(f['cluster']['features'])) > 0 and len(
-                                        (list(f['cluster']['relations']))) > 0]
-            if len(feature_with_cluster) == 0:
-                print(f'Init cluster for ' + ancestor['name'])
-                features_with_cluster = _create_initial_cluster(ancestor)
-            elif len(feature_with_cluster) == 1:
-                pass
+            print('For pair', (f1['name'], f2['name']))
+            ancestor = find_lca(f1, f2, root, features_with_cluster)
+            print('Ancestor is', ancestor['name'])
+            cs = ancestor['clusters']
+            if len(cs) == 0:
+                print(f' Initializing cluster for ' + ancestor['name'])
+                cs = _create_initial_cluster(ancestor, features_with_cluster)
             else:
-                cli.error('Incorrect amount of features. Should be 1 but is ' + str(len(feature_with_cluster)))
-                return
+                print(f' Reusing cluster for ' + ancestor['name'])
+            r = roots(f1, f2, ancestor, features_with_cluster)
+            print('Roots are', list(map(lambda x: x['name'], r)))
+            mc = _merge_sharing_elements(cs, r, features_with_cluster)
+            print('Merged cluster', mc)
+            # print('ancestor (clustered)', len(cs), ancestor['clusters'])
             break
         break
 
     return
-
-    clauses = list(map(lambda cl: list(map(lambda l: cnf.variables[abs(l)]['desc'], cl)), cnf.clauses))
-    ec_names = list(map(lambda cl: cl['name'], ecr[0]))
-    c = []
-    for clause in clauses:
-        for literal in clause:
-            if literal in ec_names:
-                c.append(clause)
-                break
-    # clauses with variable names affected by ctcs
-    clauses = c
-    del c
-    # replace feature names by actual features
-    print('clauses', clauses)
-    clauses = get_features_from_names(clauses, features)
-    print('clauses', clauses)
-    return
-    # only names
-    # print('clauses', list(map(lambda cl:list(map(lambda l: l['name'],cl)),clauses)))
-    clusters = []
-    for clause in clauses:
-        pairs = get_distinct_pairs(clause)
-        for f1, f2 in pairs:
-            pair = (f1, f2)
-            ancestor = find_lca(f1, f2, root, features)
-            print('pair', (f1['name'], f2['name']))
-            print('lca for', (f1['name'], f2['name']), 'is', ancestor['name'])
-
-        break
-
-    # for f in ec:
-
-    f1 = [x for x in features if x['name'] == 'Pasta'][0]
-    f2 = [x for x in features if x['name'] == 'SWING'][0]
-
-    lca = find_lca(f1, f2, root, features)
-
-    # print(ecr[0])
     return {
         "order": [],
         "ecr": ecr
@@ -145,14 +115,60 @@ def _create_clusters(feature):
     pass
 
 
-def _create_initial_cluster(feature):
+def _create_initial_cluster(feature, features_with_clusters):
+    clusters = []
     for child in set(feature['children']):
-
+        child = find_feature_by_name(child, features_with_clusters)
+        if not child:
+            cli.debug(f"Could not find feature {feature['name']}  in features_with_clusters")
+            return []
+        cluster = {'features': [child], 'relations': []}
+        clusters.append(cluster)
+    feature['clusters'] = clusters
     return clusters
 
 
-def _merge_sharing_elements(root, cluster):
-    pass
+def _merge_sharing_elements(clusters, r, features_wih_clusters):
+    # print("CS is", list(map(lambda x: x['name'], clusters)))
+    # print(len(clusters), "CS is", clusters)
+    # print("R is", list(map(lambda x: x['name'], r)))
+    # print("R is", roots)
+    nc = {'features': [], 'relations': []}
+    to_remove = []
+    for cl in clusters:
+        # print('cl', cl)
+        for f_in_r in r:
+            for f_in_cl in list(cl['features']):
+                if f_in_r == f_in_cl:
+                    # print('FOUND', f_in_r)
+                    # print('nc', list(nc['features']))
+                    nc['features'] = nc['features'] + cl['features']
+                    nc['relations'] = nc['relations'] + cl['relations']
+                    to_remove.append(cl)
+        break
+    clusters = [c for c in clusters if c not in to_remove]
+    clusters.append(nc)
+    # print(len(clusters), "CS is", clusters)
+    return clusters
+
+
+def roots(f1, f2, lca, features):
+    """If lca has no children, then the roots of f1 and f2 is [lca]. Otherwise roots is list containing the
+     direct children of lca which have a path to f1 or f2, respectively."""
+    children = list(lca['children'])
+    out = []
+    if len(children) == 0:
+        out.append(lca)
+    else:
+        for child in children:
+            child = find_feature_by_name(child, features)
+            path = find_path(f1, child, features)
+            if len(path) > 0 and child not in out:
+                out.append(child)
+            path = find_path(f2, child, features)
+            if len(path) > 0 and child not in out:
+                out.append(child)
+    return out
 
 
 # --------------------------------- utils ---------------------------------------------
@@ -173,6 +189,15 @@ def get_features(elem, out=None):
     for child in list(elem['children']):
         get_features(child, out)
     return feature
+
+
+def find_feature_by_name(name, features):
+    found = None
+    for f in features:
+        if f['name'] == name:
+            found = f
+            break
+    return found
 
 
 def get_features_from_names(names, features):
@@ -213,10 +238,19 @@ def calc_ecr(features, ctcs, with_features=False):
         return [], ecr
 
 
+def find_path(feature, root, features):
+    """Find path from root to current feature node. If a path exists it returns a list representing this path or
+    an empty list otherwise"""
+    path = []
+    if x := _find_path(feature, root, features, path):
+        return path
+    return []
+
+
 def _find_path(feature, root, features, parents=None):
     """
     Find the path from root to current feature node.
-    Returns True if path exists and fills parents list via side effect with nodes in the path
+    Returns True if path exists and always fills parents list via side effect with nodes in the path
     starting from the searched feature and ending at root
     """
     if len(parents) == 0:
@@ -238,7 +272,7 @@ def _find_path(feature, root, features, parents=None):
 
 
 def find_lca(f1, f2, root, features):
-    """find lowest common ancestor of two features"""
+    """find lowest common ancestor of two features. A feature can be an ancestor of itself"""
     path1 = []
     if not _find_path(f1, root, features, path1):
         cli.error(f'Could not find path in FD for feature {f1}')
@@ -253,15 +287,3 @@ def find_lca(f1, f2, root, features):
 def get_distinct_pairs(clause):
     """Returns list of distinct (1,2) == (2,1) pairs"""
     return [(a, b) for idx, a in enumerate(clause) for b in clause[idx + 1:]]
-
-
-# -----------------------data structures
-class Cluster:
-
-    def __init__(self, features, relations, **kwargs):
-        self.features = features
-        self.relations = relations
-        self.stub = "Cluster"
-
-    def __str__(self):
-        return self.stub + " " + str(self.features) + " " + str(self.relations)
