b4510a38fa9feaa44b4ace8f022170a2200722c6
Eric <elangenbucher@deloitte.com>
2022-09-13 12:49:11 +0200

    more flexible ROOT dir

--- config.py
+++ config.py
@@ -5,12 +5,13 @@ This file provides default configuration values.
 # ------------------------------------------------------------------------------
 # External imports #-----------------------------------------------------------
 from os import path
-
+import pathlib
 # ------------------------------------------------------------------------------
 
 
 # Directories
-ROOT = "/home/eric/Uni/MA/ddueruemMA"
+#ROOT = "/home/eric/Uni/MA/ddueruemMA"
+ROOT = pathlib.Path(__file__).parent
 DIR_ROOT = path.join(ROOT, "parsed")
 DIR_ARCHIVES = path.join(DIR_ROOT, "archives")
 DIR_CACHE = path.join(DIR_ROOT, "cache")
