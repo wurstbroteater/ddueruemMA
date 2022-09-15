a02c2702c76976c0e1a2b4e7ba428ef6c3ca2ff3
Eric <elangenbucher@deloitte.com>
2022-09-15 12:35:25 +0200

    updated initFM script

--- init_featureModels.sh
+++ init_featureModels.sh
@@ -3,35 +3,40 @@ rm -rf ../data/FeatureModels
 echo "[INFO] Done!"
 # Extract all Feature Models to data/FeatureModels
 echo "[INFO] Extracting backup files..."
-pushd ../data/backup > /dev/null
+pushd ../data/backup >/dev/null
 unzip -q featureModels.zip -d ./..
-popd > /dev/null
+popd >/dev/null
 echo "[INFO] Done!"
 
 echo "[INFO] Preparing models..."
-pushd ../data/FeatureModels > /dev/null
+pushd ../data/FeatureModels >/dev/null
 returnCode=0
 for D in *; do
-    if [ -d "${D}" ]; then
-        # remove everything expect files ending with model.xml
-        pushd "./${D}" > /dev/null
-        find . -type f -not -name 'model.xml' -delete
-        mv 'model.xml' "${D}.xml" 2>/dev/null
-        returnCode=$?
-        if [ $returnCode -ne 0 ]; then
-            echo "[WARN] Could not find model.xml in folder ${D}"
-        fi
-        popd > /dev/null
+  if [ -d "${D}" ]; then
+    pushd "./${D}" >/dev/null
+    ## ,, is to lower case
+    if [ "${D,,}" = "velvet" ]; then
+      mv ./HelloWorldMPL-VELVET/model.xml .
     fi
+    # remove all files expect files ending with model.xml
+    find . -type f -not -name 'model.xml' -delete
+    # remove all dirs
+    find . -type d -exec rm -rf {} + 2>/dev/null
+    mv 'model.xml' "${D}.xml" 2>/dev/null
+    returnCode=$?
+    if [ $returnCode -ne 0 ]; then
+      echo "[WARN] Could not find model.xml in folder ${D}"
+    fi
+    popd >/dev/null
+  fi
 done
-popd > /dev/null
+popd >/dev/null
 echo "[INFO] Done!"
+
 echo "[INFO] Moving into ddueruem workspace..."
-rm -rf evaluation/ > /dev/null
+rm -rf evaluation/ >/dev/null
 mkdir evaluation
 cp -r ../data/FeatureModels/* evaluation/
-# folder structure incorrect
-rm -rf evaluation/VELVET
 # RAM overflows
 rm -rf evaluation/FinancialServices01
 echo "[INFO] Done!"
