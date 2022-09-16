acd36ae65efa2e7a947a048ffa0929deeea09d6a
Eric <fleri94@web.de>
2022-09-16 22:32:44 +0200

    removed FinancialServices removal from eval

--- init_featureModels.sh
+++ init_featureModels.sh
@@ -37,7 +37,5 @@ echo "[INFO] Moving into ddueruem workspace..."
 rm -rf evaluation/ >/dev/null
 mkdir evaluation
 cp -r ../data/FeatureModels/* evaluation/
-# RAM overflows
-rm -rf evaluation/FinancialServices01
 echo "[INFO] Done!"
 echo "----Workspace is prepared!----"
